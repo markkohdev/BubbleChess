5a2f7f5f811b122c3fedb8a3e423387870f7472a
Mark Koh <markkohdev@gmail.com>
2015-03-10 03:34:38 -0400

    Added SeverHandler test cases as well as TestSuite and TestSuiteRunner.

--- BubbleChess/src/com/bubblechess/tests/BubbleChessTestSuite.java
+++ BubbleChess/src/com/bubblechess/tests/BubbleChessTestSuite.java
@@ -1,4 +1,16 @@
 package com.bubblechess.tests;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   ServerHandlerTest.class,
+   MoveValidationTest.class,
+   StateRecognitionTest.class,
+   ServerTest.class
+})
 public class BubbleChessTestSuite {
+	
 }
