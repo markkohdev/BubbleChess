74676363dd855839415743cc960024b3b1069ed7
Stephen Calabro <steve.a.calabro@gmail.com>
2015-03-10 18:25:46 -0400

    Cleaned up and added javadoc for USer

--- BubbleChess/src/com/bubblechess/client/User.java
+++ BubbleChess/src/com/bubblechess/client/User.java
@@ -5,15 +5,28 @@ public class User {
 	protected int userID;
 	protected String username;
 	
+	/**
+	 * Constructor for the User object
+	 * @param userID
+	 * @param username
+	 */
 	public User(int userID, String username){
 		this.userID = userID;
 		this.username = username;
 	}
 	
+	/**
+	 * Gets the userID of the user
+	 * @return
+	 */
 	public int getID(){
 		return userID;
 	}
 	
+	/**
+	 * Gets the username of the user
+	 * @return
+	 */
 	public String getUsername(){
 		return username;
 	}
