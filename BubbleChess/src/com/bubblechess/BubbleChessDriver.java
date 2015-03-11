4dcaa2a04dcbb5cad25c6a9f783c2738b1ba3c0c
Alexander Mann <agm52@cs.drexel.edu>
2015-03-10 20:21:43 -0400

    Fix for appWindowSwitcher

--- BubbleChess/src/com/bubblechess/BubbleChessDriver.java
+++ BubbleChess/src/com/bubblechess/BubbleChessDriver.java
@@ -24,57 +24,67 @@ public class BubbleChessDriver {
 
 		
 		while (close != 1) {
-			int gameID = 0;
-			int pane = mainAppWindow.getPaneResult();
-			switch(pane) {
-			case 1:
-				// login
-				LoginPanel newLogPanel = new LoginPanel();
-				mainAppWindow.addPanel(newLogPanel);
-				break;
-			case 2:
-				// create
-				RegisterPanel newRegister = new RegisterPanel();
-				mainAppWindow.addPanel(newRegister);
-				break;
-			case 3:
-				// mainmenu
-				MainMenuPanel newMenuPanel = new MainMenuPanel();
-				mainAppWindow.addPanel(newMenuPanel);
-				break;
-			case 4:
-				// create
-				CreateGamePanel createPanel = new CreateGamePanel();
-				mainAppWindow.addPanel(createPanel);
-				gameID = createPanel.getGameID();
-				break;
-			case 5:
-				// JoinGame
-				ArrayList<Integer> games = bridge.GetJoinableGames();
-				JoinPanel joinP = new JoinPanel();
-				mainAppWindow.addPanel(joinP);
-				break;
-			case 6:
-				// gameplay
-				WaitingForOppPanel waitingPanel = new WaitingForOppPanel(gameID);
-				mainAppWindow.addPanel(waitingPanel);
-				boolean haveOpponent = bridge.WaitForOpponent();
-				if(haveOpponent) {
-					GamePlayPanel gamePanel = new GamePlayPanel();
-					mainAppWindow.addPanel(gamePanel);
-				}
-				break;
-			case 7:
-				close = 1;
-				break;
-			case 8:
-				EndScreenPanel endPanel = new EndScreenPanel();
-				mainAppWindow.addPanel(endPanel);
-				break;
-			default:
-				close = 1;
-				break;
+			
+			appWindowSwitcher(mainAppWindow);
+		}
+	}
+	
+	/**
+	 * Function to switch windows
+	 * @param mainAppWindow
+	 */
+	public static void appWindowSwitcher(MainApplicationWindow mainAppWindow) {
+		GUIBridge bridge = mainAppWindow.getBridge();
+		int gameID = 0;
+		int pane = mainAppWindow.getPaneResult();
+		switch(pane) {
+		case 1:
+			// login
+			LoginPanel newLogPanel = new LoginPanel();
+			mainAppWindow.addPanel(newLogPanel);
+			break;
+		case 2:
+			// create
+			RegisterPanel newRegister = new RegisterPanel();
+			mainAppWindow.addPanel(newRegister);
+			break;
+		case 3:
+			// mainmenu
+			MainMenuPanel newMenuPanel = new MainMenuPanel();
+			mainAppWindow.addPanel(newMenuPanel);
+			break;
+		case 4:
+			// create
+			CreateGamePanel createPanel = new CreateGamePanel();
+			mainAppWindow.addPanel(createPanel);
+			gameID = createPanel.getGameID();
+			break;
+		case 5:
+			// JoinGame
+			ArrayList<Integer> games = bridge.GetJoinableGames();
+			JoinPanel joinP = new JoinPanel();
+			mainAppWindow.addPanel(joinP);
+			break;
+		case 6:
+			// gameplay
+			WaitingForOppPanel waitingPanel = new WaitingForOppPanel(gameID);
+			mainAppWindow.addPanel(waitingPanel);
+			boolean haveOpponent = bridge.WaitForOpponent();
+			if(haveOpponent) {
+				GamePlayPanel gamePanel = new GamePlayPanel();
+				mainAppWindow.addPanel(gamePanel);
 			}
+			break;
+		case 7:
+			close = 1;
+			break;
+		case 8:
+			EndScreenPanel endPanel = new EndScreenPanel();
+			mainAppWindow.addPanel(endPanel);
+			break;
+		default:
+			close = 1;
+			break;
 		}
 	}
 	
